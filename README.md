878a5374110c72643fc366a890920727258d5cd8
RomainJlt <romainjollivet0@gmail.com>
2024-06-20 10:50:36 +0200

    merge

---
+++ README.md
@@ -0,0 +1,7 @@
+# forum2etoile
+main.go: Contient la fonction main et les gestionnaires de routes HTTP.
+handlers.go: Contient les gestionnaires de requêtes HTTP.
+database.go: Contient les fonctions liées à la base de données.
+models.go: Contient les définitions des structures de données.
+utils.go: Contient les fonctions utilitaires comme le hachage de mot de passe.
+templates.go: Gère les opérations de templates.
---
+++ static/js/regulation.js
@@ -0,0 +1,91 @@
+let sectionPC1 = document.getElementById('sectionPC1');
+let pFleche1 = document.getElementById('pFleche1');
+let pPC1 = document.getElementById('pPC1');
+let hiddenPC1 = true;
+
+pPC1.style.display = 'none';
+
+sectionPC1.addEventListener('click', () => {
+    if (hiddenPC1) {
+        pFleche1.textContent = "▼";
+        pPC1.style.display = "block";
+        hiddenPC1 = false;
+    } else {
+        pFleche1.textContent = "►";
+        pPC1.style.display = "none";
+        hiddenPC1 = true;
+    }
+});
+
+
+
+
+let sectionPC2 = document.getElementById('sectionPC2');
+let pFleche2 = document.getElementById('pFleche2');
+let pPC2 = document.getElementById('pPC2');
+let hiddenPC2 = true;
+
+pPC2.style.display = 'none';
+
+sectionPC2.addEventListener('click', () => {
+    if (hiddenPC2) {
+        pFleche2.textContent = "▼";
+        pPC2.style.display = "block";
+        hiddenPC2 = false;
+    } else {
+        pFleche2.textContent = "►";
+        pPC2.style.display = "none";
+        hiddenPC2 = true;
+    }
+});
+
+
+
+
+
+
+
+
+
+
+let sectionCGU3 = document.getElementById('sectionCGU3');
+let pFleche3 = document.getElementById('pFleche3');
+let pCGU3 = document.getElementById('pCGU3');
+let hiddenCGU3 = true;
+
+pCGU3.style.display = 'none';
+
+sectionCGU3.addEventListener('click', () => {
+    if (hiddenCGU3) {
+        pFleche3.textContent = "▼";
+        pCGU3.style.display = "block";
+        hiddenCGU3 = false;
+    } else {
+        pFleche3.textContent = "►";
+        pCGU3.style.display = "none";
+        hiddenCGU3 = true;
+    }
+});
+
+
+
+
+
+let sectionCPD4 = document.getElementById('sectionCPD4');
+let pFleche4 = document.getElementById('pFleche4');
+let pCPD4 = document.getElementById('pCPD4');
+let hiddenCPD4 = true;
+
+pCPD4.style.display = 'none';
+
+sectionCPD4.addEventListener('click', () => {
+    if (hiddenCPD4) {
+        pFleche3.textContent = "▼";
+        pCPD4.style.display = "block";
+        hiddenCPD4 = false;
+    } else {
+        pFleche3.textContent = "►";
+        pCPD4.style.display = "none";
+        hiddenCPD4 = true;
+    }
+});
